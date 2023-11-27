176544fe81348bcda4a22e6ee98e7752109f1ba6
habuta <102156887+habuta@users.noreply.github.com>
2023-11-27 20:55:07 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-'''from __future__ import print_function
+from __future__ import print_function
 import argparse
 
 parser = argparse.ArgumentParser(description='cmdArgs')
@@ -7,28 +7,6 @@ parser.add_argument('--output', type=str, default='slack_data.csv',
 parser.add_argument('--path', required=True, type=str, help='directory where slack data reside')
 parser.add_argument('--channel', type=str, default='', help='which channel we parsing')
 parser.add_argument('--userfile', type=str, default='users.json', help='users profile information')
-
-cfg = parser.parse_args()'''
-# config.py
-
-class Config:
-    OUTPUT_FILE = 'slack_data.csv'
-    DATA_PATH = ''
-    CHANNEL = ''
-    USER_FILE = 'users.json'
-  
-# script.py
-
-from __future__ import print_function
-import argparse
-from config import Config  # Import the Config class from the config.py file
-
-parser = argparse.ArgumentParser(description='cmdArgs')
-parser.add_argument('--output', type=str, default=Config.OUTPUT_FILE,
-                    help='filename to write analysis output in CSV format')
-parser.add_argument('--path', required=True, type=str, help='directory where slack data reside')
-parser.add_argument('--channel', type=str, default=Config.CHANNEL, help='which channel we parsing')
-parser.add_argument('--userfile', type=str, default=Config.USER_FILE, help='users profile information')
 
 cfg = parser.parse_args()
 
